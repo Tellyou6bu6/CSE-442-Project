0625a1a0de6d048f8db885defe25d9d36f098beb
jundifu <jundifu@buffalo.edu>
2019-03-29 17:03:10 -0400

    updating

--- Database.php
+++ Database.php
@@ -1,8 +1,39 @@
 <?php
+	$username = "";
+	$email = "";
+	$error = array();
+
 	$db = mysql_connect('localhost', 'root', '', 'Sign_up');
 
 	if(isset($_POST['Sign_up'])){
 		$username = mysql_real_escape_string($_POST['username']);
-		$email = mysql_real_escape_string($_POST['email']);
 		$password = mysql_real_escape_string($_POST['password']);
+		$password_1 = mysql_real_escape_string($_POST['password']);
+		$email = mysql_real_escape_string($_POST['email']);
+		$phone = mysql_real_escape_string($_POST['phone'];
+
+	if(empty($username)){
+		array_push($error, "do not enter empty username")
 	}
+	if(empty($password)){
+		array_push($error, "do not enter empty password")
+	}
+	if(empty($email)){
+		array_push($error, "do not enter empty email")
+	}
+	if(empty($email)){
+		array_push($error, "do not enter empty email")
+	}
+
+	if ($password != $password_1) {
+		array_push($error, "password doesn't match")
+	}
+
+	if(count($error) == 0){
+		$password = md5($password_1);
+		$sql = "INSERT INTO users (username,password,email,phone) VALUES ('$username', '$password', '$mail', '$phone')";
+		mysqli_master_query($db, $sql);
+	}
+}
+
+?>
