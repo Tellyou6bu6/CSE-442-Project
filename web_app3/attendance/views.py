47bacf4bea0494debda7da735eb930b8f07d5e87
kwang44 <kwang44@buffalo.edu>
2019-05-03 17:38:50 -0400

    Remaking polling database Need to be abe to record is user has chosen or not

--- web_app3/attendance/views.py
+++ web_app3/attendance/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.http import HttpResponseRedirect
+from django.utils import timezone
 
-from .models import Attendance
+from .models import Attendance, Students
 
 from .models import Attendance
 # Create your views here.
@@ -15,7 +16,24 @@ def attendance(request):
     if request.method == 'POST':
         user = request.user
 
-        attendance = Attendance.objects.last()
+        attendance_object = Attendance.objects.last()
+        already_taken = attendance_object.students_set.filter(students=user).count()
+
+        end_time = attendance.end_dates
+        begin_time = attendance.begin_dates
+        current_time = timezone.now()
+
+        if already_taken == 0 and current_time <= end_time and current_time >= begin_time:
+            student_object = Students(students=user)
+
+            #attendance_object.students_set.
+
+
+
+
+
+
+
 
 
     return render(request, 'attendance/attendance.html', )
